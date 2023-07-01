161c7d6430c3075f5158d9601743578b8bc6fd52
Dario Niero <dario.niero@enginius.com>
2023-07-01 18:44:11 +0200

    add configuration for swagger

---
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/config/SpringdocConfig.java
@@ -0,0 +1,20 @@
+package com.darioniero.itsquiz.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI(){
+                return new OpenAPI().info(new Info().title("Films API")
+                .version("1.0.0").description("API to use the ITSQUiz platform"));
+    }
+}
+
