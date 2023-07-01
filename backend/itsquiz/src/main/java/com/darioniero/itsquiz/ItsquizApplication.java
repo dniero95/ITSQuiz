ca3d01df2d1eee9c79e15c1732de52f1197fd29c
Dario Niero <dario.niero@enginius.com>
2023-07-01 16:49:14 +0200

    init spring project

---
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/ItsquizApplication.java
@@ -0,0 +1,13 @@
+package com.darioniero.itsquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsquizApplication.class, args);
+	}
+
+}
---
+++ backend/itsquiz/src/test/java/com/darioniero/itsquiz/ItsquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darioniero.itsquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
