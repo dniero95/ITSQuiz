fce09b7e91d76a17fd1a64e4bdeb7ebdf9878656
Dario Niero <dario.niero@enginius.com>
2023-07-01 20:02:53 +0200

    generate QuizEntity with fields

---
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/entities/QuizEntity.java
@@ -0,0 +1,33 @@
+package com.darioniero.itsquiz.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuizEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int numberOfQuestions;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private String course;
+
+}
