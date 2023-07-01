858a2dea72cd3fe8acbdf361a0d20df7ff9f0a29
Dario Niero <dario.niero@enginius.com>
2023-07-02 01:04:10 +0200

    create methods fromQuestionToQuestionEntity and fromQuestionEntityToQuestionWithID

--- backend/itsquiz/src/main/java/com/darioniero/itsquiz/services/QuestionService.java
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/services/QuestionService.java
@@ -1,6 +1,11 @@
 package com.darioniero.itsquiz.services;
 
+import com.darioniero.itsquiz.entities.QuestionEntity;
+import com.darioniero.itsquiz.entities.QuizEntity;
 import com.darioniero.itsquiz.models.Question;
+import com.darioniero.itsquiz.models.QuestionWithIdD;
+import com.darioniero.itsquiz.models.Quiz;
+import com.darioniero.itsquiz.models.QuizWithID;
 import com.darioniero.itsquiz.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +15,24 @@ public class QuestionService {
 
     @Autowired
     QuestionRepository questionRepository;
-    public void createQuestion(Question question) {
 
+    public void createQuestion(Question question) {
+        questionRepository.save(fromQuestionToQuestionEntity(question));
     }
+
+    private QuestionEntity fromQuestionToQuestionEntity(Question question) {
+        return QuestionEntity.builder()
+                .text(question.getText())
+                .quizID(question.getQuizID())
+                .build();
+    }
+
+    private QuestionWithIdD fromQuestionEntityToQuestionWithID(QuestionEntity questionEntity) {
+        return QuestionWithIdD.builder()
+                .id(questionEntity.getId())
+                .text(questionEntity.getText())
+                .quizID(questionEntity.getQuizID())
+                .build();
+    }
+
 }
