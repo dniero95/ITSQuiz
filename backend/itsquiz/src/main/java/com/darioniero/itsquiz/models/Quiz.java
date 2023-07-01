c6dfa81c2bfac1a4c66dc6665e7dd17e3074cc30
Dario Niero <dario.niero@enginius.com>
2023-07-01 20:09:32 +0200

    add fields to quiz model

--- backend/itsquiz/src/main/java/com/darioniero/itsquiz/models/Quiz.java
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/models/Quiz.java
@@ -1,4 +1,26 @@
 package com.darioniero.itsquiz.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Quiz {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private int numberOfQuestions;
+
+    private int duration;
+
+    private String course;
+
 }
