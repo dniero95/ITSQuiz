39ac14112688e31eb3ad74acb36200ef21f26e0c
Dario Niero <dario.niero@enginius.com>
2023-07-02 22:04:32 +0200

    generate methods createAnswer in service and controller

--- backend/itsquiz/src/main/java/com/darioniero/itsquiz/controllers/AnswerController.java
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/controllers/AnswerController.java
@@ -1,9 +1,22 @@
 package com.darioniero.itsquiz.controllers;
 
+import com.darioniero.itsquiz.models.Answer;
+import com.darioniero.itsquiz.services.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/answer")
 public class AnswerController {
+
+    @Autowired
+    AnswerService answerService;
+
+    @PostMapping
+    public void createAnswer(@RequestBody Answer answer){
+        answerService.createAnswer(answer);
+    }
 }
--- backend/itsquiz/src/main/java/com/darioniero/itsquiz/services/AnswerService.java
+++ backend/itsquiz/src/main/java/com/darioniero/itsquiz/services/AnswerService.java
@@ -1,7 +1,16 @@
 package com.darioniero.itsquiz.services;
 
+import com.darioniero.itsquiz.models.Answer;
+import com.darioniero.itsquiz.repositories.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnswerService {
+
+    @Autowired
+    AnswerRepository answerRepository;
+    public void createAnswer(Answer answer) {
+
+    }
 }
